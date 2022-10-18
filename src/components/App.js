ed59004bc51f54e4daa514a46eabc2b715da28ae
salman3009 <salman444g@gmail.com>
2022-10-18 21:50:51 +0530

    code

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,32 @@ import '../styles/App.css';
 const App = () => {
   // write your code here 
 
-  const onkeyHandler=(event)=>{
-    if(event.keyCode === 13){
-      let input = document.getElementById("timeCount").value;
-      if(!isNaN(input)){
-        console.log(input);
-      }
+  const [getTime,setTime] = useState(0);
+  let timerDetails="";
 
-
+  useEffect(()=>{
+    if(getTime!=0){
+      timerDetails= setTimeout(()=>{
+        setTime(getTime-1);
+      },1000)
     }
 
+  },[getTime])
+
+  const onkeyHandler=(event)=>{
+    if(event.keyCode === 13){
+      if(timerDetails!=""){
+        clearTimeout(timerDetails);
+      }
+      let input = document.getElementById("timeCount").value;
+      if(!isNaN(input)){
+        setTime(parseInt(input));
+        console.log(input);
+      }
+      else{
+        setTime(0);
+      }
+    }
   }
 
   return (
@@ -23,7 +39,7 @@ const App = () => {
           Reverse countdown for<input id="timeCount" onKeyDown={onkeyHandler} /> sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{getTime}</div>
     </div>
   )
 }
