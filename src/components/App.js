a8c3624e33239c8b0088a6bb80c19940a738e80b
salman3009 <salman444g@gmail.com>
2022-10-18 21:23:03 +0530

    code

--- src/components/App.js
+++ src/components/App.js
@@ -2,46 +2,28 @@ import React, { Component, useState, useEffect } from "react";
 import '../styles/App.css';
 
 const App = () => {
-
-  const [getTime,setTime]=useState(0);
-  let result='';
   // write your code here 
 
-  useEffect(()=>{
-    if(getTime!=0){
-      result = setTimeout(()=>{
-        setTime(getTime-1);
-      },1000)
+  const onkeyHandler=(event)=>{
+    if(event.keyCode === 13){
+      let input = document.getElementById("timeCount").value;
+      if(!isNaN(input)){
+        console.log(input);
+      }
+
+
     }
 
-   
-  },[getTime])
-
-  const onKeyboardEnter=(event)=>{
-      if(event.keyCode === 13){
-        let input = document.getElementById("timeCount").value;
-        console.log(input!=0);
-        if(!isNaN(input) && input!=0){
-          console.log(input);
-          setTime(parseInt(input));
-        }
-        else{
-          setTime(0);
-          if(result!="")[
-            clearTimeout(result)
-          ]
-        }
-      } 
   }
 
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={onKeyboardEnter} /> sec.
+          Reverse countdown for<input id="timeCount" onKeyDown={onkeyHandler} /> sec.
         </h1>
       </div>
-      <div id="current-time">{getTime}</div>
+      <div id="current-time">{/* remaining time */}</div>
     </div>
   )
 }
